b42d7fadb27a9b97ad9005f14d9f7a5286c45885
Janidu.SmartSystems <janidu.smartsystems@gmail.com>
2026-06-14 13:13:48 +0530

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/smartsystems_Fileuploader/',
   plugins: [react(), tailwindcss()],
 })
